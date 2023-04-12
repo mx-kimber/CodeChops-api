6f9615946d7f85d7f73d0abe14236f03d3524a78
Kimber Ogden <mx.kimber@gmail.com>
2023-04-12 15:00:20 -0500

    add before_actions to controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,4 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,4 @@
 class SkillsController < ApplicationController
+  before_action :set_skill, only: [:show, :edit, :update, :destroy]
+
 end
