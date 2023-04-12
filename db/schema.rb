5c6023cdf8c0f81ce7ebc17a8f39dda2e54dd767
Kimber Ogden <mx.kimber@gmail.com>
2023-04-12 13:57:04 -0500

    add skills conroller and gem rack-cors, update application controller to config middleware - create model associations

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 ActiveRecord::Schema[7.0].define(version: 2023_04_12_170544) do
-  # These are extensions that must be enabled in order to support this database
+
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
