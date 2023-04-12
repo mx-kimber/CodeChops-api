17428f4079bdb6a1954573012a8b6f679842697c
Kimber Ogden <mx.kimber@gmail.com>
2023-04-12 15:33:47 -0500

    create CRUD in both controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,38 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @category = Category.all
+    render :index
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @category = Category.new(
+      name: name = params[:name] = params[:name]
+    )
+
+    @category.save
+    render :show
+  end
+
+  def update
+    @category = Category.find_by(id: params[:id])
+    @category.name = params[:name] || @category.name
+
+    @category.save
+    rendr :show
+  end
+
+  def destroy
+    @category = Category.find_by(id: params[:id])
+    @category.destroy
+    render json: {message: "Category successfully destroyed."}
+#   redirect_to * *
+  end
+
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,39 @@
-class SkillsController < ApplicationController
+class CategoriesController < ApplicationController
   before_action :set_skill, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @skill = Skill.all
+    render :index
+  end
+
+  def show
+    @skill = Skill.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @skill = Skill.new(
+      name: name = params[:name] = params[:name]
+    )
+
+    @skill.save
+    render :show
+  end
+
+  def update
+    @skill = Skill.find_by(id: params[:id])
+    @skill.name = params[:name] || @skill.name
+
+    @skill.save
+    rendr :show
+  end
+
+  def destroy
+    @skill = Skill.find_by(id: params[:id])
+    @skill.destroy
+    render json: {message: "skill successfully destroyed."}
+#   redirect_to * *
+  end
+
 end
+
