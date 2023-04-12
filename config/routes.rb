12623e90dff62046488d02c90e7a3bff700bc2a1
Kimber Ogden <mx.kimber@gmail.com>
2023-04-12 14:19:00 -0500

    create routes, add exception to  application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # front end (?)
+  get "/codechops" => "codechops#index"
+  get "/codechops/:id" => "codechops#show"
+
+  # categories
+  get "/categories" => "categories#index"
+  get "/categories/:id" => "categories#show"
+  post "categories/" => "categories#create"
+  patch "/categories/:id" => "categories#update"
+  delete "/categories/:id" => "categories#destroy"
+
+  # skills
+  get "/skills" => "skills#index"
+  get "skills/:id" => "skills#show"
+  post "skills/" => "skills#create"
+  patch "/skills/:id" => "skills#update"
+  delete "/skills/:id" => "skills#destroy"
+
+
 end
+
